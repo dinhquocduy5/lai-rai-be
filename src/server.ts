95aead3abdf2ccded1dad252e8b82fff12f3794c
Duy Dinh Quoc <dinhquocduy5@gmail.com>
2025-11-01 13:38:49 +0700

    Register CORS middleware and allow frontend & local dev origins (with credentials)

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,16 @@ import 'module-alias/register';
 import { env } from '@/config/env';
 import { logger } from '@/utils/logger';
 import { app } from './app';
+import cors from 'cors';
 
 app.listen(env.PORT, () => {
   logger.info(`🚀 Server running at http://localhost:${env.PORT}`);
 });
+
+app.use(cors({
+  origin: [
+    'https://lai-rai-fe.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
